c8465b17d472a7df190840494479fb30b3f6c6a6
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2022-03-16 00:37:29 +0100

    Macro measure initial implementation.

--- metrics-fn-codegen/src/lib.rs
+++ metrics-fn-codegen/src/lib.rs
@@ -1,8 +1,56 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		let result = 2 + 2;
-		assert_eq!(result, 4);
+use std::any::Any;
+
+use proc_macro::TokenStream;
+use proc_macro2::{Delimiter, Group, Ident, TokenTree};
+use quote::{quote_spanned, ToTokens};
+
+mod fn_signature;
+
+use fn_signature::*;
+
+#[proc_macro_attribute]
+pub fn measure(attr: TokenStream, item: TokenStream) -> TokenStream {
+	let attr = proc_macro2::TokenStream::from(attr);
+	let item = proc_macro2::TokenStream::from(item);
+
+	let span = proc_macro2::Span::call_site();
+
+	let signature = FnSignature::from(item.clone());
+	println!("{:#?}", signature);
+	println!("{:#?}", signature.argument_names());
+	println!(
+		"{:#?}",
+		signature.call(&["a".to_string(), "b".to_string(), "c".to_string()])
+	);
+
+	let wrapper = wrap_method(item);
+
+	let output = quote_spanned! { span =>
+		#wrapper
+	};
+
+	output.into()
+}
+
+fn wrap_method(stream: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+	let mut tokens = Vec::new();
+	let mut iter = stream.clone().into_iter();
+	while let Some(token) = iter.next() {
+		tokens.push(token);
 	}
+
+	proc_macro2::TokenStream::from_iter(tokens.into_iter())
+}
+
+fn get_signature(stream: proc_macro2::TokenStream) -> proc_macro2::TokenStream {
+	let mut iter = stream.into_iter();
+	while let Some(token) = iter.next() {
+		if let TokenTree::Group(group) = token {
+			if group.delimiter() == Delimiter::Parenthesis {
+				return group.into_token_stream();
+			}
+		}
+	}
+
+	panic!("Parameter list not found.");
 }
