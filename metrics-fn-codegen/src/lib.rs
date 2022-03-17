d83eaaba87ca57b20b1edbecd7a091840f755511
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2022-03-18 00:57:56 +0100

    Parse the fn body in the Function struct.

--- metrics-fn-codegen/src/lib.rs
+++ metrics-fn-codegen/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use proc_macro2::{Delimiter, TokenStream as TokenStream2, TokenTree};
+use proc_macro2::{TokenStream as TokenStream2, TokenTree};
 use quote::quote_spanned;
 
 mod function;
@@ -18,36 +18,30 @@ pub fn measure(_attr: TokenStream, item: TokenStream) -> TokenStream {
 	// parse the fn.
 	let fn_attributes = get_fn_attributes(item.clone());
 	let fn_signature = Function::from(item.clone());
-	let fn_body = get_fn_body(item.clone());
 
 	// map the fn to wrapper and wrapped variables
 	let wrapper_attributes = fn_attributes;
 	let wrapper_signature = fn_signature;
 	let wrapped_signature = wrapper_signature.rename("wrapped".to_owned());
-	let wrapped_body = fn_body;
 
 	// build the wrapped fn.
-	let output = wrap(wrapper_attributes, wrapper_signature, wrapped_signature, wrapped_body);
+	let output = wrap(wrapper_attributes, wrapper_signature, wrapped_signature);
 
 	println!("{}", output.to_string());
 
 	output.into()
 }
 
-fn wrap(
-	wrapper_attributes: TokenStream2,
-	wrapper_signature: Function,
-	wrapped_signature: Function,
-	wrapped_body: TokenTree,
-) -> TokenStream2 {
+fn wrap(wrapper_attributes: TokenStream2, wrapper_signature: Function, wrapped_signature: Function) -> TokenStream2 {
 	let span = proc_macro2::Span::call_site();
 
+	let wrapped_fn = wrapped_signature.body.clone();
 	let wrapped_call = wrapped_signature.call(wrapped_signature.argument_names().as_slice());
 
 	let output = quote_spanned! { span =>
 		#wrapper_attributes
 		#wrapper_signature {
-			#wrapped_signature #wrapped_body
+			#wrapped_signature #wrapped_fn
 
 			let start__ = std::time::Instant::now();
 			let output__ = #wrapped_call;
@@ -76,28 +70,4 @@ fn get_fn_attributes(stream: TokenStream2) -> TokenStream2 {
 	}
 
 	TokenStream2::from_iter(tokens.into_iter())
-}
-
-fn get_fn_body(stream: TokenStream2) -> TokenTree {
-	let mut iter = stream.into_iter();
-
-	// skip until the 'fn' keyword.
-	while let Some(token) = iter.next() {
-		if let TokenTree::Ident(ident) = token {
-			if ident == "fn" {
-				break;
-			}
-		}
-	}
-
-	// skip until a group with a brace delimiter.
-	while let Some(token) = iter.next() {
-		if let TokenTree::Group(group) = &token {
-			if group.delimiter() == Delimiter::Brace {
-				return token;
-			}
-		}
-	}
-
-	panic!("Function body not found.");
 }
