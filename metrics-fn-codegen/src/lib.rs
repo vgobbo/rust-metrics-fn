c7aea1df464b17f731311df8a6a96b2a2787b36a
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2022-03-18 02:36:19 +0100

    Better variable names.

--- metrics-fn-codegen/src/lib.rs
+++ metrics-fn-codegen/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use proc_macro2::{TokenStream as TokenStream2, TokenTree};
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote_spanned;
 
 mod function;
@@ -16,37 +16,37 @@ pub fn measure(_attr: TokenStream, item: TokenStream) -> TokenStream {
 	let item = TokenStream2::from(item);
 
 	// parse the fn.
-	let fn_signature = Function::from(item.clone());
+	let function = Function::from(item.clone());
 
 	// map the fn to wrapper and wrapped variables
-	let wrapper_signature = fn_signature;
-	let wrapped_signature = wrapper_signature.rename("wrapped".to_owned());
+	let wrapper_fn = function;
+	let wrapped_fn = wrapper_fn.rename("wrapped".to_owned());
 
 	// build the wrapped fn.
-	let output = wrap(wrapper_signature, wrapped_signature);
+	let output = wrap(wrapper_fn, wrapped_fn);
 
 	println!("{}", output.to_string());
 
 	output.into()
 }
 
-fn wrap(wrapper_signature: Function, wrapped_signature: Function) -> TokenStream2 {
+fn wrap(wrapper_fn: Function, wrapped_fn: Function) -> TokenStream2 {
 	let span = proc_macro2::Span::call_site();
 
-	let wrapper_attributes = TokenStream2::from_iter(wrapper_signature.attributes.clone().into_iter());
-	let wrapped_fn = wrapped_signature.body.clone();
-	let wrapped_call = wrapped_signature.call(wrapped_signature.argument_names().as_slice());
+	let wrapper_attributes = TokenStream2::from_iter(wrapper_fn.attributes.clone().into_iter());
+	let wrapped_body = wrapped_fn.body.clone();
+	let wrapped_call = wrapped_fn.call(wrapped_fn.argument_names().as_slice());
 
 	let output = quote_spanned! { span =>
 		#wrapper_attributes
-		#wrapper_signature {
-			#wrapped_signature #wrapped_fn
+		#wrapper_fn {
+			#wrapped_fn #wrapped_body
 
 			let start__ = std::time::Instant::now();
 			let output__ = #wrapped_call;
 			let end__ = std::time::Instant::now();
 
-			log::info!("Time {:?}", end__.duration_since(start__));
+			println!("Time {:?}", end__.duration_since(start__));
 
 			return output__;
 		}
