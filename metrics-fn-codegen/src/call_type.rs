ae4f86d2332f252a634af0dcdaeca88df99dea96
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2022-03-19 02:16:09 +0100

    Enum CallType.

---
+++ metrics-fn-codegen/src/call_type.rs
@@ -0,0 +1,58 @@
+use std::fmt::{Display, Formatter};
+
+use syn::FnArg::Receiver;
+use syn::Signature;
+
+pub enum CallType {
+	None,
+	OwnedSelf,
+	ReferenceSelf,
+	OwnedMutableSelf,
+	ReferenceMutableSelf,
+}
+
+impl From<&Signature> for CallType {
+	fn from(sig: &Signature) -> Self {
+		let receiver_opt = sig
+			.inputs
+			.iter()
+			.filter_map(|arg| {
+				if let Receiver(receiver) = arg {
+					Some(receiver)
+				} else {
+					None
+				}
+			})
+			.next();
+
+		if let Some(receiver) = receiver_opt {
+			if receiver.mutability.is_some() {
+				if receiver.reference.is_some() {
+					CallType::ReferenceMutableSelf
+				} else {
+					CallType::OwnedMutableSelf
+				}
+			} else {
+				if receiver.reference.is_some() {
+					CallType::ReferenceSelf
+				} else {
+					CallType::OwnedSelf
+				}
+			}
+		} else {
+			CallType::None
+		}
+	}
+}
+
+impl Display for CallType {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		match self {
+			CallType::None => write!(f, "None"),
+			CallType::OwnedSelf => write!(f, "OwnedSelf"),
+			CallType::ReferenceSelf => write!(f, "ReferenceSelf"),
+			CallType::OwnedMutableSelf => write!(f, "OwnedMutableSelf"),
+			CallType::ReferenceMutableSelf => write!(f, "ReferenceMutableSelf"),
+		}
+	}
+}
