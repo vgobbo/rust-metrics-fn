17b77dfb62683ca782c77943f204c5d748e781fb
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2022-03-21 01:18:42 +0100

    Implemented record function.

--- metrics-fn-log/src/lib.rs
+++ metrics-fn-log/src/lib.rs
@@ -1,8 +1,13 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		let result = 2 + 2;
-		assert_eq!(result, 4);
-	}
+use log::info;
+
+pub fn record<T>(module: &str, result: Result<(), T>, elapsed_ns: f64)
+where
+	T: ToString,
+{
+	let result_text = match result {
+		Ok(_) => "Ok".to_owned(),
+		Err(value) => value.to_string(),
+	};
+
+	info!("{} {} {}", module, result_text, elapsed_ns);
 }
