47d70a7928859bdde1eb406dc4dcae8ae2b6c44d
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2022-03-22 00:54:39 +0100

    Record metrics in histogram.

--- metrics-fn-prometheus/src/lib.rs
+++ metrics-fn-prometheus/src/lib.rs
@@ -1,6 +1,25 @@
+use lazy_static::lazy_static;
+use prometheus::{register_histogram_vec, HistogramVec};
+
+lazy_static! {
+	static ref APPLICATION_METHOD_TIMINGS: HistogramVec = register_histogram_vec!(
+		"application_method_timings",
+		"Method execution timings in second.",
+		&["mod", "fn", "res"],
+		vec![0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,]
+	)
+	.unwrap();
+}
+
 pub fn record<T>(module: &str, result: &Result<(), T>, elapsed_s: f64)
 where
 	T: ToString,
 {
-	todo!()
+	let result_text = match result {
+		Ok(_) => "Ok".to_owned(),
+		Err(value) => value.to_string(),
+	};
+
+	let labels = &[module, "", result_text.as_str()];
+	APPLICATION_METHOD_TIMINGS.with_label_values(labels).observe(elapsed_s);
 }
