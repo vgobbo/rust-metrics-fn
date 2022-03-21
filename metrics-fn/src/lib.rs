950e121b48944ddc7e2ec3aa3d33102b56834482
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2022-03-21 23:39:24 +0100

    Prepare to allow multiple backends to be available.

--- metrics-fn/src/lib.rs
+++ metrics-fn/src/lib.rs
@@ -1,9 +1,10 @@
 pub use metrics_fn_codegen::measure;
 
-#[cfg(feature = "log")]
 pub fn record<T>(module: &str, result: Result<(), T>, elapsed_ns: f64)
 where
 	T: ToString,
 {
-	metrics_fn_log::record(module, result, elapsed_ns);
+	if cfg!(feature = "log") {
+		metrics_fn_log::record(module, result, elapsed_ns);
+	}
 }
